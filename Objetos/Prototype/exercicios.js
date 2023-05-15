fdff753a92708fae174b9cb64f3504b471aaf951
studioValenz <studiovalenz@gmail.com>
2023-05-15 15:38:43 -0300

    Exercicio resolvido da aula de prototype

---
+++ Objetos/Prototype/exercicios.js
@@ -0,0 +1,39 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+function Pessoas(nome, sobrenome, idade) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+}
+
+Pessoas.prototype.nomeCompleto = function () {
+  return "Nome Completo: " + this.nome + " " + this.sobrenome;
+};
+
+const esposa = new Pessoas("Kevene", "Taina", 22);
+
+console.log(esposa.nomeCompleto());
+
+// Liste os métodos acessados por
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+console.log(Object.getOwnPropertyNames(Document.prototype));
+console.log(Object.getOwnPropertyNames(NodeList.prototype));
+console.log(Object.getOwnPropertyNames(HTMLCollection.prototype));
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector("li");
+li; // HTMLLIElement
+li.click; // function
+li.innerText; // string
+li.value; // number
+li.hidden; // boolean
+li.offsetLeft; // number
+li.click(); // error
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; // boolean
