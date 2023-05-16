a6404689d69c4707723df657418b63af0047ce42
studioValenz <studiovalenz@gmail.com>
2023-05-16 15:16:47 -0300

    metodos de strings 2

--- Objetos/Strings/script.js
+++ Objetos/Strings/script.js
@@ -23,7 +23,25 @@ console.log(fruta.endsWith("na"));
 
 console.log(fruta.slice(3, 6));
 
-// metodo indexOf = retorna o index da string, assim que achar o primeiro resultado ele ja retorna
+/** metodo indexOf = retorna o index da string, assim que achar o primeiro resultado
+ *  ele ja retorna */
 
 console.log(fruta.indexOf("n"));
 console.log(fruta.indexOf("a"));
+
+/** metodo replace = Troca parte da string por outra. Podemos utilizar uma regular
+expression ou um valor direto. Se usarmos um valor direto ele irá
+trocar apenas o primeiro valor que encontrar. */
+
+let listaItens = "Camisas Bonés Calças Bermudas Vestidos Saias";
+listaItens = listaItens.replace(/[ ]+/g, ",");
+console.log(listaItens);
+
+let preco = "R$ 1200,43";
+preco = preco.replace(",", ".");
+console.log(preco);
+
+// metodo split = Divide a string de acordo com o padrão passado e retorna uma array.
+
+const arrayList = listaItens.split(", ");
+console.log(arrayList);
