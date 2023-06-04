53c84246a8363c3c053f30109116bdb9a842c784
studioValenz <studiovalenz@gmail.com>
2023-06-04 14:06:34 -0300

    Aula sobre Picker - react native

---
+++ Picker_RN/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import { Picker } from "@react-native-picker/picker";
+
+export default function App() {
+  const [carroSelecionado, setCarroSelecionado] = useState(0);
+  const [carros, setCarros] = useState([
+    { key: 1, nome: "Golf 1.6", valor: 62.0 },
+    { key: 2, nome: "Saveiro 1.6", valor: 29.3 },
+    { key: 3, nome: "Gol 1.0", valor: 25.2 },
+    { key: 4, nome: "BMW 120i", valor: 109.0 },
+    { key: 5, nome: "Uno 1.0", valor: 25.0 },
+  ]);
+
+  let carrosItem = carros.map((v, k) => {
+    return <Picker.Item key={k} value={k} label={v.nome} />;
+  });
+
+  return (
+    <View style={styles.container}>
+      <Picker
+        selectedValue={carroSelecionado}
+        onValueChange={(itemValue, itemIndex) => setCarroSelecionado(itemValue)}
+      >
+        {carrosItem}
+      </Picker>
+
+      <Text style={styles.carros}>Carro: {carros[carroSelecionado].nome} </Text>
+      <Text style={styles.carros}>
+        R$: {carros[carroSelecionado].valor.toFixed(3)}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 35,
+  },
+  carros: {
+    marginTop: 15,
+    fontSize: 25,
+  },
+});
